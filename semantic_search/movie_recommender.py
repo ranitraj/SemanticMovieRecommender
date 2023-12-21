594338942ddd9d598b42fb5a7ce3c32533b15264
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2023-12-21 16:44:37 -0700

    Implementing mechanism to generate vector embeddings using HuggingFace Inference API

--- semantic_search/movie_recommender.py
+++ semantic_search/movie_recommender.py
@@ -1,5 +1,8 @@
 import os
 import pymongo
+import requests
+
+import urls
 
 from dotenv import load_dotenv
 
@@ -9,16 +12,35 @@ load_dotenv()
 # Load Local environment variables
 mongo_atlas_uri = os.getenv('URI_MOVIE_RECOMMENDER')
 mongo_atlas_password = os.getenv('PASSWORD_MOVIE_RECOMMENDER')
+hugging_face_api_token = os.getenv('HUGGING_FACE_TOKEN')
 
 # Replace password in uri-template with the password env variable
 movie_recommender_uri = mongo_atlas_uri.replace('<password>', mongo_atlas_password)
 
-# Initialize PyMongo Client & Database
+# Part-1: Initialize PyMongo Client & database
 client = pymongo.MongoClient(movie_recommender_uri)
 movie_database = client.sample_mflix
 
-# Test Connection & Print results
-movies_collection = movie_database.movies.find().limit(2)
-for cur_movie in movies_collection:
-    print(cur_movie)
+# Get 'movies' collection from movie_database
+movies_collection = movie_database.movies
+
+
+# Part-2: Get Vector-Embedding from a Sentence using HuggingFace sentence-transformers 'all-MiniLM-L6-v2' model via AP
+def get_vector_embeddings(payload: str) -> list[float]:
+    print(f"Generating Vector Embeddings for text == ${payload}")
+
+    api_response = requests.post(
+        urls.API_GENERATE_EMBEDDINGS,
+        headers={"Authorization": f"Bearer {hugging_face_api_token}"},
+        json={"inputs": payload}
+    )
+
+    if api_response.status_code != 200:
+        raise ValueError(f"Request failed with Status-Code: {api_response.status_code} & Message: {api_response.text}")
+
+    return api_response.json()
+
+
+# Print Embeddings
+print(get_vector_embeddings("Hello World!"))
 
---
+++ semantic_search/urls.py
@@ -0,0 +1,2 @@
+API_GENERATE_EMBEDDINGS = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
